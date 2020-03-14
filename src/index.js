a8dc060e52734165115501e51f0367a00e61703d
Yulia Harbacheuskaya <yulia.harbacheuskaya@gmail.com>
2020-03-14 20:56:14 +0300

    feat: add expression calculation for medium tests

--- src/index.js
+++ src/index.js
@@ -1,35 +1,39 @@
 const OPERATIONS = {'*': 1, '/': 1, '+': 2, '-': 2};
+
+function top(array, pos = 1) {
+    return array[array.length - pos];
+}
+
 function eval(result) {
     // Do not use eval!!!
     let value = 0;
     let temp = [];
-    let i = 0;
     while (result.length > 0) {
-        if (! (result[0] in OPERATIONS)) {
-            temp.push(Number(result[i]));
+        if (!(result[0] in OPERATIONS)) {
+            temp.push(Number(result[0]));
         }
         switch (result[0]) {
             case '+':
-                value = temp[temp.length - 2] + temp[temp.length - 1];
+                value = top(temp, 2) + top(temp);
                 temp.splice(temp.length - 2, 2, value);
                 break;
             case '-':
-                value = temp[temp.length - 2] - temp[temp.length - 1];
+                value = top(temp, 2) - top(temp);
                 temp.splice(temp.length - 2, 2, value);
                 break;
             case '*':
-                value = temp[temp.length - 2] * temp[temp.length - 1];
+                value = top(temp, 2) * top(temp);
                 temp.splice(temp.length - 2, 2, value);
                 break;
             case '/':
                 if (temp[1] === 0) {
                     throw Error("TypeError: Division by zero.");
                 }
-                value = temp[temp.length - 2] / temp[temp.length - 1];
+                value = top(temp, 2) / top(temp);
                 temp.splice(temp.length - 2, 2, value);
                 break;
         }
-        result.splice(0,1);
+        result.splice(0, 1);
     }
     return value;
 }
@@ -38,25 +42,42 @@ function expressionCalculator(expr) {
     // write your solution here
     let result = Array();
     let operations = Array();
-    expr = expr.match(/(\d+|[+\/\-*])/g);
-    expr.forEach( elem => {
-        if (!(elem  in OPERATIONS)) {
+    let last_bracket_flag = 0;
+    expr = expr.match(/(\d+|[+\/\-*()])/g);
+    expr.forEach(elem => {
+        if (!(elem in OPERATIONS) && elem !== '(' && elem !== ')') {
             result.push(elem);
-        }
-        else if (operations.length === 0 || OPERATIONS[elem] < OPERATIONS[operations[operations.length - 1]]) {
+        } else if (elem === '(') {
             operations.push(elem);
-        }
-        else if (OPERATIONS[elem] > OPERATIONS[operations[operations.length - 1]]) {
-            result = result.concat(operations.reverse());
-            operations = [];
+            last_bracket_flag = result.length;
+        } else if (elem === ')') {
+            let array = result.splice(last_bracket_flag, result.length - last_bracket_flag);
+            let temp = operations.splice(operations.lastIndexOf('(') + 1, operations.length - operations.lastIndexOf('(') - 1);
+            array = array.concat(temp.reverse());
+            result.push(eval(array));
+            if (operations.length === 0) {
+                throw Error("ExpressionError: Brackets must be paired");
+            }
+            operations.pop();
+        } else if (operations.length === 0 || OPERATIONS[elem] < OPERATIONS[top(operations)] || top(operations) === '(') {
             operations.push(elem);
-        }
-        else {
+        } else if (OPERATIONS[elem] > OPERATIONS[top(operations)]) {
+            if (operations.lastIndexOf('(') > -1) {
+                result = result.concat(operations.splice(operations.lastIndexOf('(') + 1, operations.length - operations.lastIndexOf('(') - 1).reverse());
+            } else {
+                result = result.concat(operations.reverse());
+                operations = [];
+            }
+            operations.push(elem);
+        } else {
             result.push(operations.pop());
             operations.push(elem);
         }
     });
-    result= result.concat(operations.reverse());
+    result = result.concat(operations.reverse());
+    if (result.includes('(')) {
+        throw Error("ExpressionError: Brackets must be paired");
+    }
     return eval(result);
 }
 
